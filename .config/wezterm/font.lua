4ff17eaf4ce8773b78ce8ef2dcde4e72e879ae16
laptop <vineetkr.shr@gmail.com>
2023-03-13 16:46:08 +0530

    first

---
+++ .config/wezterm/font.lua
@@ -0,0 +1,21 @@
+local wezterm = require 'wezterm'
+
+local font_size = 12
+local bold = false
+local font_family = ({
+    'JetBrainsMono Nerd Font', -- [1]
+    'BlexMono Nerd Font', -- [2]
+    'Liga SFMono Nerd Font', -- [3]
+    'Iosevka Nerd Font Mono', -- [4]
+    'FiraCode Nerd Font Mono', -- [6]
+    'ComicCodeLigatures', -- [6]
+    'Nouveau IBM', -- [7]
+    'IBM Plex Mono' -- [8]
+})[1]
+
+local options = {}
+if bold then options['weight'] = 'Bold' end
+
+local font = wezterm.font(font_family, options)
+
+return {font = font, font_size = font_size}
---
+++ .config/wezterm/keybinds.lua
@@ -0,0 +1,73 @@
+local wezterm = require 'wezterm'
+
+local M = {
+    -- Useful default binds:
+    -- Ctrl + Shift + x => ActivateCopyMode
+    -- Ctrl + Shift + z => TogglePaneZoomState
+    -- Ctrl + Shift + space => QuickSelect
+    keys = {
+        {key = 'f', mods = 'CTRL', action = wezterm.action.TogglePaneZoomState},
+        {
+            key = 'n',
+            mods = 'ALT',
+            action = wezterm.action.SplitHorizontal {
+                domain = 'CurrentPaneDomain'
+            }
+        },
+        { key = 'J', mods = 'ALT', action = wezterm.action.DecreaseFontSize },
+        { key = 'K', mods = 'ALT', action = wezterm.action.IncreaseFontSize },
+        { key = 'C', mods = 'ALT', action = wezterm.action.Copy },
+        {
+            key = 'n',
+            mods = 'SHIFT|ALT',
+            action = wezterm.action.SplitVertical {domain = 'CurrentPaneDomain'}
+        },
+        {
+            key = 'h',
+            mods = 'SHIFT|ALT',
+            action = wezterm.action.AdjustPaneSize {"Left", 1}
+        },
+        {
+            key = 'l',
+            mods = 'SHIFT|ALT',
+            action = wezterm.action.AdjustPaneSize {"Right", 1}
+        },
+        {
+            key = 'j',
+            mods = 'SHIFT|ALT',
+            action = wezterm.action.AdjustPaneSize {"Down", 1}
+        },
+        {
+            key = 'k',
+            mods = 'SHIFT|ALT',
+            action = wezterm.action.AdjustPaneSize {"Up", 1}
+        },
+        {
+            key = 'h',
+            mods = 'ALT',
+            action = wezterm.action.ActivatePaneDirection 'Left'
+        },
+        {
+            key = 'l',
+            mods = 'ALT',
+            action = wezterm.action.ActivatePaneDirection 'Right'
+        },
+        {
+            key = 'j',
+            mods = 'ALT',
+            action = wezterm.action.ActivatePaneDirection 'Down'
+        },
+        {
+            key = 'k',
+            mods = 'ALT',
+            action = wezterm.action.ActivatePaneDirection 'Up'
+        },
+        {
+            key = 't',
+            mods = 'CTRL',
+            action = wezterm.action.SpawnTab 'DefaultDomain'
+        }
+    }
+}
+
+return M
