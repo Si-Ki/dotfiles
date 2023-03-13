714d24707dedbba0aba54bdcc655093db902bd90
si-ki <vineetkr.shr@gmail.com>
2023-03-13 13:36:28 +0530

    auto

---
+++ .config/nvim/after/plugin/rosepine.lua
@@ -0,0 +1,12 @@
+function set_color(color)
+  color = color or "rose-pine"
+  vim.cmd.colorscheme(color)
+
+  vim.api.nvim_set_hl(0, "Normal", { bg
+= "none" })
+vim.api.nvim_set_hl(0, "NormalFloat",
+{ bg = "none" })
+
+end
+
+set_color()
