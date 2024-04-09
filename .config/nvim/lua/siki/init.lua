8889aaba99b9dfc7aecd2a55c7c46fc7d95038c6
Si-Ki <vineetkr.shr@gmail.com>
2024-04-09 15:04:00 +0530

    auto

--- .config/nvim/lua/siki/init.lua
+++
@@ -1,2 +0,0 @@
-require("siki.set")
-require("siki.remap")
--- .config/wezterm/misc.lua
+++
@@ -1,7 +0,0 @@
-local M = {
-    default_cursor_style = 'SteadyUnderline',
-    enable_wayland = true,
-    default_prog = {"zsh"}
-}
-
-return M
