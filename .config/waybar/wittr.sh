25d2cb06fdfcf9084789fe2c9ae7058c3d8e41ac
si-ki <vineetkr.shr@gmail.com>
2025-05-25 13:43:22 +0530

    auto

---
+++ .config/waybar/wittr.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+for i in {1..5}
+do
+    text=$(curl -s "https://wttr.in/$1?format=1")
+    if [ $? -eq 0 ]
+    then
+        text=$(echo "$text" | awk '{print $2}')
+        tooltip=$(curl -s "https://wttr.in/$1?format=4")
+        if [ $? -eq 0 ]
+        then
+            tooltip=$(echo "$tooltip" | sed -E "s/\s+/ /g")
+            echo "{\"text\":\"$text\", \"tooltip\":\"$tooltip\"}"
+            exit
+        fi
+    fi
+    sleep 2
+done
+echo "{\"text\":\"error\", \"tooltip\":\"error\"}"
